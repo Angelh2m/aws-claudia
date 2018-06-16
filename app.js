579ac60875f7348549b444730a502574b4677290
Angel Hernandez <ahm@Angels-MBP.home>
2018-06-16 16:39:50 -0400

    Lambda

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var ApiBuilder = require('claudia-api-builder'),
+const ApiBuilder = require('claudia-api-builder'),
+
     api = new ApiBuilder();
 
 module.exports = api;
@@ -6,3 +7,9 @@ module.exports = api;
 api.get('/hello', function() {
     return 'hello world';
 });
+
+// INSATALL: claudia create --region us-east-1 --api-module app
+// CREATE: npm install claudia-api-builder -S 
+// DEPLOY: claudia create --region us-east-1 --api-module app
+// https://claudiajs.com/tutorials/hello-world-api-gateway.html
+// "https://j8xiuyokv3.execute-api.us-east-1.amazonaws.com/latest"
